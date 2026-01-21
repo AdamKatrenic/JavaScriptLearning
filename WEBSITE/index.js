596935a9ca294f9cfb904051f2285a7f6e509eda
AdamKatrenic <adamkatrenic@gmail.com>
2026-01-21 17:25:53 +0100

    Callbacks

--- WEBSITE/index.js
+++ WEBSITE/index.js
@@ -1,48 +1,16 @@
-//Random password Generator
+//Callbacks
 
-function generatePassword(lenght, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+sum(displayPage, 5, 10);
 
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numberChars = '0123456789';
-    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
-
-    let allowedChars = '';
-    let password = '';
-
-    allowedChars += includeLowercase ? lowercaseChars : '';
-    allowedChars += includeUppercase ? uppercaseChars : '';
-    allowedChars += includeNumbers ? numberChars : '';
-    allowedChars += includeSymbols ? symbolChars : '';
-
-    if (lenght <= 0) {
-        return 'Password length must be greater than 0';
-    }
-    if (allowedChars.length === 0) {
-        return 'At least one character type must be selected';
-    }
-
-    for(let i = 0; i < lenght; i++) {
-        const randomIndex = Math.floor(Math.random() * allowedChars.length);
-        password += allowedChars[randomIndex];
-    }
-
-    return password;
+function sum(callback, a, b) {
+    let result = a + b;
+    callback(result);
 }
 
-const passwordLength = 10;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
+function displaySum(result) {
+    console.log("The sum is: " + result);
+}
 
-const password = generatePassword
-(
-    passwordLength, 
-    includeLowercase, 
-    includeUppercase, 
-    includeNumbers, 
-    includeSymbols
-);
-
-console.log("Generated Password: " + password);
+function displayPage(result){
+    document.getElementById("myH1").textContent = "The sum is: " + result;
+}
